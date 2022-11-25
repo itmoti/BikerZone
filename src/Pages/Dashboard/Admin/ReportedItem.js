de769c49e1c5b5576b61fae217b52178412b1ea8
itmoti <motiarrahman9707@gmail.com>
2022-11-26 01:31:53 +0600

    reported to admin page implimented

--- src/Pages/Dashboard/Admin/ReportedItem.js
+++ src/Pages/Dashboard/Admin/ReportedItem.js
@@ -7,10 +7,49 @@ const ReportedItem = () => {
     queryKey : ['reportedItems'] , 
     queryFn : () =>  fetch(`http://localhost:5000/dashboard/reportedItems`).then(res => res.json())
 })
-console.log(reportedItems)
+const handleDeleteBtn =(id) => {
+   fetch(`http://localhost:5000/dashboard/reportedproducts/${id}` , {
+    method : 'DELETE'
+   })
+   .then(res => res.json())
+   .then(data => {
+    console.log(data)
+    refetch()
+   })
+}
     return (
         <div>
-            reported item
+             <div className="overflow-x-auto">
+            <table className="table table-zebra w-full">
+            
+              <thead>
+                <tr>
+                  <th></th>
+                  <th>Product Name</th>
+                  <th>Price</th>
+                  <th>Uploader</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+              {reportedItems?.map((items ,index) =>
+                
+                <tr key={index}>
+                  <th>{index +1}</th>
+                  <td>{items.ProductName}</td>
+                  <td>{items.ResellPrice}</td>
+                  <td>{items.SellerName}</td>
+                  <td>
+                    <button className='btn btn-error btn-sm' onClick={() => handleDeleteBtn(items._id)}>Delete</button>
+                  </td>
+                </tr>
+                )}
+                
+              
+              
+              </tbody>
+            </table>
+          </div>
         </div>
     );
 };
