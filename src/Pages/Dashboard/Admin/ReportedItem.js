800d0a15d3797fce6c34b2a9f5b6cef3d14666b1
itmoti <motiarrahman9707@gmail.com>
2022-11-25 17:26:40 +0600

    fetched reported items

--- src/Pages/Dashboard/Admin/ReportedItem.js
+++ src/Pages/Dashboard/Admin/ReportedItem.js
@@ -1,6 +1,13 @@
-import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import React, { useEffect } from 'react';
 
 const ReportedItem = () => {
+
+   const {data : reportedItems , refetch} = useQuery({
+    queryKey : ['reportedItems'] , 
+    queryFn : () =>  fetch(`http://localhost:5000/dashboard/reportedItems`).then(res => res.json())
+})
+console.log(reportedItems)
     return (
         <div>
             reported item
--- src/Pages/Home/Catagories/Products.js
+++ src/Pages/Home/Catagories/Products.js
@@ -23,6 +23,17 @@ const Products = () => {
                     setProducts(data)
         })
      }, [])
+
+     const handleReportToAdminBtn =(id) => {
+        console.log(id)
+        fetch(`http://localhost:5000/catagory/product/${id}` , {
+            method: 'PUT'
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+        })
+     }
 console.log(products)
     return (
         <div>
@@ -58,6 +69,8 @@ console.log(products)
                                    
                                     <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" ></path></svg>
                                 </label>
+
+                                <button className='btn btn-primary' onClick={() => handleReportToAdminBtn(product._id)} >Report To admin</button>
                             </div>
                         </div>
 
