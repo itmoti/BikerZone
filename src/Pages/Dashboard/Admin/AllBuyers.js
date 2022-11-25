d9845d53eb0b843ef3ef5e064261dadce9fc2fb0
itmoti <motiarrahman9707@gmail.com>
2022-11-25 12:36:39 +0600

    buyers and sellers deleting function implimented

--- src/Pages/Dashboard/Admin/AllBuyers.js
+++ src/Pages/Dashboard/Admin/AllBuyers.js
@@ -2,17 +2,58 @@ import {  useQuery } from '@tanstack/react-query';
 import React from 'react';
 
 const AllBuyers = () => {
-    const {data : buyers} = useQuery({
+    const {data : buyers , refetch} = useQuery({
         queryKey : ['buyers'] , 
         queryFn : () =>  fetch(`http://localhost:5000/dashboard/allBuyers`).then(res => res.json())
     
     
       })
-      console.log(buyers)
+      const  handleDeleteBtn  = (id) => {
+        fetch(`http://localhost:5000/dashboard/allBuyers/${id}` , {
+            method : 'DELETE' ,
+          
+
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            refetch()
+        })
+     }
     return (
         <div>
-            All buyersjhhhhhhhhhhh
-        </div>
+         
+        <div className="overflow-x-auto">
+        <table className="table table-zebra w-full">
+        
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Email</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+          {buyers?.map((buyer ,index) =>
+            
+            <tr key={index}>
+              <th>{index +1}</th>
+              <td>{buyer.name}</td>
+              <td>{buyer.email}</td>
+              <td>
+                <button onClick={() => handleDeleteBtn(buyer._id)}>Delete</button>
+              </td>
+            </tr>
+            )}
+            
+          
+          
+          </tbody>
+        </table>
+      </div>
+        
+               </div>
     );
 };
 
--- src/Pages/Dashboard/Admin/AllSellers.js
+++ src/Pages/Dashboard/Admin/AllSellers.js
@@ -2,15 +2,56 @@ import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 
 const AllSellers = () => {
-    const {data : buyers} = useQuery({
+    const {data : sellers , refetch} = useQuery({
         queryKey : ['Sellers'] , 
         queryFn : () =>  fetch(`http://localhost:5000/dashboard/allSellers`).then(res => res.json())
     })
-      console.log(buyers)
+     const  handleDeleteBtn  = (id) => {
+        fetch(`http://localhost:5000/dashboard/allSellers/${id}` , {
+            method : 'DELETE' ,
+          
+
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            refetch()
+        })
+     }
     return (
         <div>
-            all sellers
-        </div>
+         
+            <div className="overflow-x-auto">
+            <table className="table table-zebra w-full">
+            
+              <thead>
+                <tr>
+                  <th></th>
+                  <th>Name</th>
+                  <th>Email</th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+              {sellers?.map((buyer ,index) =>
+                
+                <tr key={index}>
+                  <th>{index +1}</th>
+                  <td>{buyer.name}</td>
+                  <td>{buyer.email}</td>
+                  <td>
+                    <button onClick={() => handleDeleteBtn(buyer._id)}>Delete</button>
+                  </td>
+                </tr>
+                )}
+                
+              
+              
+              </tbody>
+            </table>
+          </div>
+            
+                   </div>
     );
 };
 
